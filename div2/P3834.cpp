cea1520c271491c9ae0ff8ce48f27c3ba856332f
nayix <jinsoyun141@gmail.com>
2020-09-16 13:01:33 +0800

    P3834 P3919

---
+++ div2/P3834.cpp
@@ -0,0 +1,75 @@
+/*
+P3834: 可持久化线段树 2（主席树）
+https://www.luogu.com.cn/problem/P3834
+*/
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+const int _N = 2e5 + 5;
+const int _M = 4e6 + 5;
+int _cnt, _ls[_M], _rs[_M], _si[_M], _root[_N];
+unordered_map<int, int> _key, _val;
+
+struct ARR {
+	int p, val;
+} _arr[_N];
+
+int update(int cur, int l, int r, int tar) {
+	int x = ++_cnt;
+	_si[x] = _si[cur] + 1;
+	if (l != r) {
+		int mid = l + r >> 1;
+		if (tar <= mid) {
+			_ls[x] = update(_ls[cur], l, mid, tar);
+			_rs[x] = _rs[cur];
+		} else {
+			_ls[x] = _ls[cur];
+			_rs[x] = update(_rs[cur], mid+1, r, tar);
+		}
+	}
+	return x;
+}
+
+int query(int lef, int rig, int l, int r, int k) {
+	if (l == r) return _val[l];
+	int si = _si[_ls[rig]] - _si[_ls[lef]];
+	if (k <= si) return query(_ls[lef], _ls[rig], l, l + r >> 1, k);
+	return query(_rs[lef], _rs[rig], (l + r >> 1) + 1, r, k - si);
+}
+
+inline void solve() {
+	register int i, n, m, l, r, k, cnt = 0;
+	cin >> n >> m;
+	for (i = 1; i <= n; ++i) {
+		_arr[i].p = i;
+		cin >> _arr[i].val;
+	}
+	sort(_arr + 1, _arr + n + 1, [](ARR x, ARR y) {
+		return x.val < y.val;
+	});
+	_val[++cnt] = _arr[1].val;
+	_key[_arr[1].val] = cnt;
+	for (i = 2; i <= n; ++i) {
+		if (_arr[i].val == _arr[i-1].val) continue;
+		_val[++cnt] = _arr[i].val;
+		_key[_arr[i].val] = cnt;
+	}
+	sort(_arr + 1, _arr + n + 1, [](ARR x, ARR y) {
+		return x.p < y.p;
+	});
+	for (i = 1; i <= n; ++i) _root[i] = update(_root[i-1], 1, cnt, _key[_arr[i].val]);
+	while (m--) {
+		cin >> l >> r >> k;
+		cout << query(_root[l-1], _root[r], 1, cnt, k) << '\n';
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	solve();
+	return 0;
+}
---
+++ div3/P3919.cpp
@@ -0,0 +1,64 @@
+/*
+P3919: 可持久化线段树 1（可持久化数组）
+https://www.luogu.com.cn/problem/P3919
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int _N = 1e6 + 5;
+int _cnt, _root[_N];
+int _ls[_N*20], _rs[_N*20], _val[_N*20];
+
+int build(int l, int r) {
+	int x = ++_cnt;
+	if (l == r) cin >> _val[x];
+	else {
+		int mid = l + r >> 1;
+		_ls[x] = build(l, mid);
+		_rs[x] = build(mid+1, r);
+	}
+	return x;
+}
+
+int update(int cur, int l, int r, int tar) {
+	int x = ++_cnt;
+	if (l == r) cin >> _val[x];
+	else {
+		int mid = l + r >> 1;
+		if (tar <= mid) {
+			_ls[x] = update(_ls[cur], l, mid, tar);
+			_rs[x] = _rs[cur];
+		} else {
+			_ls[x] = _ls[cur];
+			_rs[x] = update(_rs[cur], mid+1, r, tar);
+		}
+	}
+	return x;
+}
+
+int query(int cur, int l, int r, int tar) {
+	if (l == r) return _val[cur];
+	int mid = l + r >> 1;
+	if (tar <= mid) return query(_ls[cur], l, mid, tar);
+	return query(_rs[cur], mid+1, r, tar);
+}
+
+inline void solve() {
+	register int i, n, m, v, t, loc;
+	cin >> n >> m;
+	_root[0] = build(1, n);
+	for (i = 1; i <= m; ++i) {
+		cin >> v >> t >> loc;
+		if (t == 1) _root[i] = update(_root[v], 1, n, loc);
+		else cout << query(_root[i] = _root[v], 1, n, loc) << '\n';
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	solve();
+	return 0;
+}
